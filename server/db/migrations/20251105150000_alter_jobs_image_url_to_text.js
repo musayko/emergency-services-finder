91ed552d31ef4b045867752e02017a3117b8cf12
musayko <musa.ozdemir@hotmail.comclear>
2025-11-05 16:54:52 +0200

    feat: alter image_url column to text type

---
+++ server/db/migrations/20251105150000_alter_jobs_image_url_to_text.js
@@ -0,0 +1,19 @@
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+exports.up = function(knex) {
+  return knex.schema.alterTable('jobs', function(table) {
+    table.text('image_url').alter();
+  });
+};
+
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+exports.down = function(knex) {
+  return knex.schema.alterTable('jobs', function(table) {
+    table.string('image_url').alter();
+  });
+};
